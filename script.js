1c6f3d454a2033286fd3d159f9ea329b5919c824
Megan Murphy <childovhurin@gmail.com>
2020-02-29 00:23:05 -0500

    added main variables and password elements

---
+++ script.js
@@ -0,0 +1,33 @@
+//Variables
+var confirmUpper;
+var confirmLower;
+var confirmNumerical;
+var confirmSpecial;
+
+//Password elements
+upper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+numerical = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+special = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
+
+//First Click Event
+function generatePassword() {
+
+}
+
+
+//prompt for password length enter between 8 or 128
+
+//upper case letters ok or cancel
+
+//lower case letters ok or cancel
+
+//numerical ok or cancel
+
+//characters ok or cancel
+
+//generate password
+
+
+
+
